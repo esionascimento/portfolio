42bb85786916a0e038902172c8389fb90f1171fc
esionascimento <esionascimento@proton.me>
2022-09-25 22:02:01 -0400

    refactor: rename axios -> axiosGithub

--- src/service/axiosGithub.ts
+++ src/service/axiosGithub.ts
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 const HOST_GITHUB = 'https://api.github.com/users/esionascimento';
 
-const axiosInstance = axios.create({
+const axiosInstanceGithub = axios.create({
   baseURL: HOST_GITHUB,
 });
 
-axiosInstance.interceptors.response.use(
+axiosInstanceGithub.interceptors.response.use(
   (response) => response,
   (error) => Promise.reject(error.response || error)
 );
 
-export default axiosInstance;
+export default axiosInstanceGithub;
