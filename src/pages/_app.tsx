bf0d6b19f44cce899fd00ca5cf035455a8c7b51c
esionascimento <esionascimento@proton.me>
2022-10-09 01:59:08 -0400

    refactor: _app adicionado ThemeProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,11 @@
+import ThemeProvider from 'src/theme';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
