50a0b8a402bc4693423526cc997704e34662a809
esionascimento <esionascimento@proton.me>
2022-10-10 17:24:20 -0400

    refactor: config para Welcome

--- src/config/navbar/index.tsx
+++ src/config/navbar/index.tsx
@@ -1,1 +1,11 @@
-export const pages = ['Home', 'Skills', 'Sobre mim'];
+export const configTabs = ['Home', 'Skills', 'Sobre mim'];
+import WorkIcon from '@mui/icons-material/Work';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+export const configWelcome = [
+  { text: 'Desenvolvedor Full Stack', icon: <WorkIcon sx={{ mr: 1 }} /> },
+  { text: 'Porto Velho - RO', icon: <LocationOnIcon sx={{ mr: 1 }} /> },
+  {
+    text: 'Sou um desenvolvedor de software autodidata e muito dedicado com experiência na construção de sites e APIs Rest.',
+  },
+];
