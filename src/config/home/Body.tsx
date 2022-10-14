9b248f32d0797bafb810c455f9ab1f1cdd5b1170
esionascimento <esionascimento@proton.me>
2022-10-14 00:09:17 -0400

    create: config home

---
+++ src/config/home/Body.tsx
@@ -0,0 +1,30 @@
+export const configCardWork = [
+  {
+    title: 'Repositório',
+    text: 'Projeto criado com NextJS, Material UI, TypeScript',
+    chip: ['ReactJS', 'NextJS', 'TypeScript'],
+    image: '/image/foodnas.webp',
+    reposity: 'https://github.com/esionascimento/portfolio',
+  },
+  {
+    title: 'FoodNas',
+    text: 'Gestor de Pedidos - Utilizando API do Ifood, criado Frond e Back-end',
+    chip: ['ReactJS', 'NodeJS'],
+    image: '/image/foodnas.webp',
+    reposity: 'https://github.com/esionascimento/foodNas',
+  },
+  {
+    title: 'Calculadora de Tintas',
+    text: 'Simular a quantidade de tinta nescessario para pintar uma parede.',
+    chip: ['ReactJS'],
+    image: '/image/simular-tinta.webp',
+    reposity: 'https://github.com/esionascimento/desafioDigitalRepublic',
+  },
+  {
+    title: 'Agenda Telefonica',
+    text: 'Simular uma agenda telefonica, Aplicação desenvolvido com ReactJS',
+    chip: ['ReactJS'],
+    image: '/image/agenda-telefonica.webp',
+    reposity: 'https://github.com/esionascimento/desafioKlever',
+  },
+];
