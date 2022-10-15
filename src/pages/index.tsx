e610f2f0bf4be2e544a492c3f9523d2d1f9995cc
esionascimento <esionascimento@proton.me>
2022-10-15 17:51:00 -0400

    refactor: home, add icon Scroll, footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,61 @@
 import * as React from 'react';
 import NavbarHorizontal from 'src/components/Navbar';
-import { pages } from 'src/config/navbar';
-import { Welcome } from 'src/sections/home';
+import { configTabs } from 'src/config/navbar';
+import { Welcome, Work } from 'src/sections/home';
+import { Box, Fab, Fade, Toolbar, useScrollTrigger } from '@mui/material';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { Footer } from 'src/components/Footer';
 
-const Home = () => {
+function ScrollTop(props) {
+  const { children, window } = props;
+
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    const anchor = ((event.target as HTMLDivElement).ownerDocument || document).querySelector(
+      '#back-to-top-anchor'
+    );
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        block: 'center',
+      });
+    }
+  };
+
+  return (
+    <Fade in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: 'fixed', bottom: 16, right: 16 }}
+      >
+        {children}
+      </Box>
+    </Fade>
+  );
+}
+
+const Home = (props: any) => {
   return (
     <>
-      <NavbarHorizontal pages={pages} />
-
+      <NavbarHorizontal pages={configTabs} />
+      <Toolbar id="back-to-top-anchor" />
       <Welcome />
+
+      <Work />
+
+      <ScrollTop {...props}>
+        <Fab size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
+
+      <Footer />
     </>
   );
 };
