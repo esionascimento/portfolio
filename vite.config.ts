b4bedb3aeda7afd067f6098a0d2cb79d51434817
esionascimento <esionascimento@proton.me>
2024-11-27 08:29:31 -0400

    refactor: vite.config port server 3020

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'node:path';
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      {
+        find: /^~(.+)/,
+        replacement: path.join(process.cwd(), 'node_modules/$1'),
+      },
+      {
+        find: /^src(.+)/,
+        replacement: path.join(process.cwd(), 'src/$1'),
+      },
+    ],
+  },
+  server: {
+    port: 3020,
+  },
+  preview: {
+    port: 3020,
+  },
+});
