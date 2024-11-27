2adfc0a2dc7d226756e8f1b09c822ca72ce26ae2
esionascimento <esionascimento@proton.me>
2024-11-27 16:11:09 -0400

    refactor: App import ThemeProvider, CssBaseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
 import PageHome from './pages';
 import PageLayoutDefault from './layouts/LayoutDefault';
+import { createTheme } from './theme';
 import './App.css';
 
 function App() {
+  const theme = createTheme();
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<PageLayoutDefault />}>
-            <Route
-              path="/"
-              element={<PageHome />}
-            />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <Routes>
+            <Route element={<PageLayoutDefault />}>
+              <Route
+                path="/"
+                element={<PageHome />}
+              />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   );
 }
