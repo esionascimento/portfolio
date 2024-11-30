45f818c7257641637a39c87c418962d2342300ab
esionascimento <esionascimento@proton.me>
2024-11-30 14:01:03 -0400

    refactor: index pages home add locash hash

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,66 @@
+import { useEffect, useRef } from 'react';
+import { Container } from '@mui/material';
+import { useLocation } from 'react-router-dom';
+
 import { AboutMeSection } from '../sections/home/AboutMe';
+import { ContactSection } from '../sections/home/ContactSection';
 import { EducationSection } from '../sections/home/Education';
 import { HomeSection } from '../sections/home/Home';
+import { ProjectsShowcaseSection } from '../sections/home/ProjectsShowcase';
+import { RodapeSection } from '../sections/home/Rodape';
 
 const PageHome = () => {
+  const homeRef = useRef<HTMLDivElement>(null);
+  const sobreMimRef = useRef<HTMLDivElement>(null);
+  const projetosRef = useRef<HTMLDivElement>(null);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash === '#home') {
+      return homeRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }
+    if (location.hash === '#sobre-mim') {
+      return sobreMimRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }
+    if (location.hash === '#projetos') {
+      return projetosRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [location]);
+
   return (
     <>
-      <HomeSection />
-      <AboutMeSection />
-      <EducationSection />
+      <div
+        ref={homeRef}
+        id="home"
+      >
+        <HomeSection />
+      </div>
+
+      <Container maxWidth="lg">
+        <div
+          ref={sobreMimRef}
+          id="sobre-mim"
+          style={{
+            display: 'flex',
+          }}
+        >
+          <AboutMeSection />
+        </div>
+        <EducationSection />
+        <div
+          ref={projetosRef}
+          id="projetos"
+          style={{
+            display: 'flex',
+          }}
+        >
+          <ProjectsShowcaseSection />
+        </div>
+      </Container>
+
+      <ContactSection />
+      <RodapeSection />
     </>
   );
 };
