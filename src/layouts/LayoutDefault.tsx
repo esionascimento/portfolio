42dc3369a1462cdd6936c0a773a90e05142a498b
esionascimento <esionascimento@proton.me>
2025-03-14 20:55:22 -0400

    refactor: LayoutDefault move code

--- src/layouts/LayoutDefault.tsx
+++ src/layouts/LayoutDefault.tsx
@@ -1,5 +1,8 @@
 import { ReactNode } from 'react';
-import { AppBar, Box, Button, Link, Toolbar, useMediaQuery, useTheme } from '@mui/material';
+import { useMediaQuery, useTheme } from '@mui/material';
+
+import { HeaderDesktopLayoutDefault } from './components/HeaderDesktop';
+import { TemporaryDrawer } from './components/HeaderMobile';
 
 interface LayoutProps {
   children?: ReactNode;
@@ -11,59 +14,8 @@ const LayoutDefault = ({ children }: LayoutProps) => {
 
   return (
     <>
-      {smUp && (
-        <AppBar
-          position="fixed"
-          sx={{ backgroundColor: '#000' }}
-        >
-          <Toolbar>
-            <Box sx={{ flexGrow: 1 }}>
-              <Link href="/">
-                <Button color="inherit">Nascimento</Button>
-              </Link>
-            </Box>
-            <Box>
-              <Link href="/#home">
-                <Button
-                  color="inherit"
-                  sx={{ marginLeft: 2 }}
-                >
-                  Home
-                </Button>
-              </Link>
-              <Link href="/#sobre-mim">
-                <Button
-                  color="inherit"
-                  sx={{ marginLeft: 2 }}
-                >
-                  Sobre mim
-                </Button>
-              </Link>
-              <Link href="/#projetos">
-                <Button
-                  color="inherit"
-                  sx={{ marginLeft: 2 }}
-                >
-                  Projetos
-                </Button>
-              </Link>
-              <a
-                target="_blank"
-                href="https://api.whatsapp.com/send/?phone=5569992577460&text=Olá&type=phone_number&app_absent=0"
-              >
-                <Button
-                  id="button-vamos-conversar"
-                  variant="contained"
-                  color="primary"
-                  sx={{ marginLeft: 2 }}
-                >
-                  Vamos conversar
-                </Button>
-              </a>
-            </Box>
-          </Toolbar>
-        </AppBar>
-      )}
+      {smUp && <HeaderDesktopLayoutDefault />}
+      {!smUp && <TemporaryDrawer />}
 
       {children}
     </>
